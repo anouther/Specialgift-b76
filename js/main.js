ef4de4857caa0ccd628e8b803d625d43e813b16c
anouther <155570107+anouther@users.noreply.github.com>
2025-07-26 14:30:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Happy 5th Mensiversary, love! ').split('');
+        const titles = ('kita udah 6 bulan pacaran, serius? :D').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Mas nggak nyangka kita udah selama ini sayang! Mas sayang banget sama kamu! Mas kasi bunga ini buat pacar mas yang paling cantik, bunganya warna pink biar cantik kaya kamu. Semoga kita selalu bareng dan bisa ketemu ditanggal 14 lainnya sayang! ";
+            subtitle.innerText = "happy sixth months mensive kakak!! semoga kita bisa lama-lama bareng ya, thank you for being a good boyfriend, my dearest salvias. <3";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
